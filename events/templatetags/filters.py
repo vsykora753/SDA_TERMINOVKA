95bda37f14fa29480aec187d073b06f6cb087aaa
vsykora753 <vladimir.sykora@centrum.cz>
2025-05-30 07:10:19 +0000

    add filtr  change value in m -> to km

--- events/templatetags/filters.py
+++ events/templatetags/filters.py
@@ -8,3 +8,10 @@ def truncate_without_ellipsis(value, length):
     if len(value) > length:
         return value[:length]
     return value
+    
+@register.filter
+def m_2_km(value):
+    try:
+        return value / 1000
+    except (ValueError, TypeError):
+        return value
