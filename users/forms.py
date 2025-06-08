6dd025026237a028f7a53510093685376c203afe
vsykora753 <vladimir.sykora@centrum.cz>
2025-06-08 11:12:33 +0200

    edit forms for formatting

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
-from datetime import date, datetime
+from events.models import Event
+from datetime import date
 from dateutil.relativedelta import relativedelta
 from django.contrib.auth import authenticate
 
@@ -69,7 +70,7 @@ class RegisterForm(UserCreationForm):
 
     def clean_birth_date(self):
         birth_date = self.cleaned_data.get('birth_date')
-        today = datetime.date.today()
+        today = date.today()
         if not birth_date:
             raise forms.ValidationError("Datum narození je povinné.")
 
@@ -126,4 +127,80 @@ class OrganizerRegisterForm(UserCreationForm):
         if commit:
             user.save()
         return user
+    
+class OrganizerEventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = [
+            'date_event',
+            'name_event',
+            'description',
+            'start_time',
+            'distance',
+            'country',
+            'city',
+            'region',
+            'typ_race',
+            'propozition',
+            'start_fee',
+        ]
+        labels = {
+            'date_event': 'Datum události',
+            'name_event': 'Název události',
+            'description': 'Popis události',
+            'start_time': 'Čas startu',
+            'distance': 'Vzdálenost',
+            'country': 'Země',
+            'city': 'Město',
+            'region': 'Kraj',
+            'typ_race': 'Typ závodu - povrch',
+            'propozition': 'Propozice',
+            'start_fee': 'Startovné',    
+        }
+
+
+    def clean_date_event(self):
+        date_event = self.cleaned_data.get('date_event')
+        if date_event and date_event < date.today():
+            raise forms.ValidationError(
+                "Datum události nemůže být v minulosti. "
+                "Zvolte prosím platné datum."
+            )
+        return date_event
+    
+    def clean_start_time(self):
+        start_time = self.cleaned_data.get('start_time')
+        if start_time:
+            if (start_time.hour < 0 or start_time.hour > 23 or 
+                start_time.minute < 0 or start_time.minute > 59
+            ):
+                raise forms.ValidationError(
+                    "Čas startu musí být v platném formátu (HH:MM)."
+                )
+        return start_time
+
+    def clean_distance(self):
+        distance = self.cleaned_data.get('distance')
+        if distance is not None and distance <= 0:
+            raise forms.ValidationError(
+                "Vzdálenost musí být kladné číslo(v metrech)."
+            )
+        return distance
+    
+    def clean_start_fee(self):
+        start_fee = self.cleaned_data.get('start_fee')
+        if start_fee is not None and start_fee < 0:
+            raise forms.ValidationError(                
+                "Startovné nemůže být záporné číslo."
+            )
+        if start_fee is not None:
+            rounded_fee = round(start_fee/ 10) * 10
+            return rounded_fee
+        
+        return start_fee
+    def clean_region(self):
+        region = self.cleaned_data.get('region')
+        if not region:
+            raise forms.ValidationError("Kraj je povinný.")
+        return region
     
