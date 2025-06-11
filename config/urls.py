04c896f48d7bc71266d960ef4cd16177be6cee07
vsykora753 <vladimir.sykora@centrum.cz>
2025-06-11 05:40:38 +0200

    urls-py ->> OrganizerLoginView ->RoleBasedLoginView

--- config/urls.py
+++ config/urls.py
@@ -15,58 +15,77 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-import debug_toolbar
+
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path,include
+from django.urls import path, include
 
 # importy pohledu pro  zobrazení událostí, registrace uživatelů a organizátorů
 
 from events.views import EventListView,TerminovkaView,EventDetailView,EventRegisterView
 from users.views import UserRegisterView, OrganizerRegisterView
 from users.views import user_dashboard, organizer_dashboard
-from users.views import UserLoginView, OrganizerLoginView
-from users.views import UserRegistrationSuccessView
+from users.views import UserRegistrationSuccessView,RoleBasedLoginView
 from users.views import OrganizerRegistrationSuccessView,UserLogoutView
 from users.views import OrganizerEventListView,OrganizerEventCreateView
 from users.views import OrganizerEventEditView,OrganizerEventDeleteView
 
-if settings.DEBUG:
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-] 
+
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-   
+
     # Události
     path('', EventListView.as_view(), name='events_list'),
     path('terminovka/', TerminovkaView.as_view(), name='events_search'),
     path('<int:pk>/', EventDetailView.as_view(), name='event_details'),
-     path('<int:pk>/prihlasit/', EventRegisterView.as_view(), name='event_register'),
+    path('<int:pk>/prihlasit/', EventRegisterView.as_view(), name='event_register'),
 
 
     # Uživatelské účty
     path('register/user/', UserRegisterView.as_view(), name='user_register'),
-    path('registrace/uzivatel/uspesna/', UserRegistrationSuccessView.as_view(), name='user_registration_success'),
-    path('login/user/', UserLoginView.as_view(), name='user_login'),
+    path('registrace/uzivatel/uspesna/', UserRegistrationSuccessView.as_view(),
+        name='user_registration_success'),
+
+    path('login/user/', RoleBasedLoginView.as_view(
+        template_name='user/user_login.html'),
+        name='user_login'),
+
     path('user/dashboard/', user_dashboard, name='user_dashboard'),    
     path('user/logout/', UserLogoutView.as_view(), name='user_logout'),
 
 
     # Organizátoři
-    path('register/organizer/', OrganizerRegisterView.as_view(), name='organizer_register'),
-    path('login/organizer/', OrganizerLoginView.as_view(), name='organizer_login'), 
-    path('registrace/organizator/uspesna/', OrganizerRegistrationSuccessView.as_view(), name='organizer_registration_success'),
-    path('organizer/dashboard/', organizer_dashboard, name='organizer_dashboard'),
-    path('organizer/logout/', UserLogoutView.as_view(), name='organizer_logout'),
+    path('register/organizer/', OrganizerRegisterView.as_view(),
+        name='organizer_register'),
+
+    path('login/organizer/', RoleBasedLoginView.as_view(
+        template_name='organizer/organizer_login.html'),
+        name='organizer_login'),
+
+    path('registrace/organizator/uspesna/', 
+        OrganizerRegistrationSuccessView.as_view(),
+        name='organizer_registration_success'),
+
+    path('organizer/dashboard/', organizer_dashboard,
+        name='organizer_dashboard'),
+
+    path('organizer/logout/', UserLogoutView.as_view(),
+        name='organizer_logout'),
 
     # Udalosti organizátora
-    path('organizer/events/',OrganizerEventListView.as_view(), name='organizer_event_list'),
-    path('organizer/events/create/', OrganizerEventCreateView.as_view(), name='organizer_event_create'),
-    path('organizer/events/<int:pk>/update/', OrganizerEventEditView.as_view(), name='organizer_event_update'), 
-    path('organizer/events/<int:pk>/delete/', OrganizerEventDeleteView.as_view(), name='organizer_event_delete'),
+    path('organizer/events/',OrganizerEventListView.as_view(),
+        name='organizer_event_list'),
+
+    path('organizer/events/create/', 
+        OrganizerEventCreateView.as_view(), name='organizer_event_create'),
+
+    path('organizer/events/<int:pk>/update/', OrganizerEventEditView.as_view(),
+        name='organizer_event_update'), 
+
+    path('organizer/events/<int:pk>/delete/', 
+        OrganizerEventDeleteView.as_view(), name='organizer_event_delete'),
 
 ]
 
