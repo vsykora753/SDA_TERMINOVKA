6cdca2372945f0eb217c2c9651849dd37755aaef
vsykora753 <vladimir.sykora@centrum.cz>
2025-06-11 20:43:28 +0000

    create RegistrationListView

--- registrations/views.py
+++ registrations/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Registration
 
-# Create your views here.
+
+class RegistrationListView(ListView):
+    """
+    View for displaying a list of registrations for a specific event.
+    """ 
+
+    model = Registration
+    template_name = 'registration_list.html'
+    context_object_name = 'registrations'
+
+    def get_queryset(self):
+        """
+        Returns the queryset of registrations for the specific event.
+        """
+        event_id = self.kwargs.get('event_id')
+        return Registration.objects.filter(id_event_id=event_id).select_related('id_user' ).order_by('id_user__last_name')
+
+        
